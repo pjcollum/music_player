48db3def82d0a5047b9e7ff1b4ef22e9cef990a7
Peter Collum <petercollum@hotmail.co.uk>
2020-06-03 17:11:35 +0100

    added playlist

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const nextSong = document.getElementById('nextSong');
 
 const progressBar = document.querySelector('#progress-bar'); // element where progress bar appears
 
+let isPlayingFromPlaylist = false;
+
 //Songs Array
 class Song {
     constructor(songName, songArtist) {
@@ -31,24 +33,30 @@ let songLibrary = [
     FaterLee = new Song("Fater Lee", "Black Ant")
 ]
 
-let currentSongIndex = 0;
+let playlistArray = []
 
-function playSong(){
-    audio.src = Song[songIndex];  //set the source of 0th song 
-    //songTitle.textContent = songs[currentSong]; // set the title of song
-    audio.play();    // play the song
-}
+let currentSongIndex = 0;
+let currentPlaylistSongIndex = 0;
 
 //Create list of songs
 const createSongList = () => {
-    const list = document.createElement('ol');
+    const list = document.createElement('ul');
 
     for(let i = 0; i < songLibrary.length; i++) {
         const item = document.createElement('li');
         const button = document.createElement('button');
-        button.onclick = () => {playSong(i)};
+
+        //add checkbox to each song name to add to playlist
+        var x = document.createElement("INPUT");
+        x.setAttribute("type", "checkbox");
+        x.setAttribute("value", i)
+        x.setAttribute("name", "songName")
+
+        button.onclick = () => {playSong(i, [...songLibrary])};
         button.innerHTML = songLibrary[i].songName;
         //item.appendChild(document.createTextNode(`${JSON.stringify(songLibrary[i].songName)} by ${JSON.stringify(songLibrary[i].songArtist)}`))
+       
+        item.appendChild(x)
         item.appendChild(button)
         list.appendChild(item)
     }
@@ -58,42 +66,88 @@ document.getElementById('songList').appendChild(createSongList());
 
 //Search Audio File
 document.getElementById('searchBar').addEventListener('input' ,function(){
-    console.log(this.value.toLowerCase());        //print text input to console
+    //console.log(this.value.toLowerCase());        //print text input to console
     //console.log(songLibrary[this.value - 1].songName);
 
     let result = songLibrary.filter(s => s.songName.toLowerCase().includes(this.value.toLowerCase()));
-    
-    const list = document.createElement('ol');
+    const list = document.createElement('ul');
 
     for(let i = 0; i < result.length; i++) {
         const item = document.createElement('li');   
+
         const button = document.createElement('button');
-        button.onclick = () => {playSong(i)};
+        button.onclick = () => {playSong(i, [...result])};
         button.innerHTML = result[i].songName;
+       // item.appendChild(x)
         item.appendChild(button)
         list.appendChild(item)
     }
     
-    document.getElementById('songList').innerHTML = ""; 
-    document.getElementById('songList').appendChild(list); 
+    document.getElementById('songList').innerHTML = "";         //clears list of songs
+    document.getElementById('songList').appendChild(list);      //shows the matching song in a list, in place of all songs
 })
 
+//let playlistArray = [];
+
+//Create To Playlist
+document.getElementById('addToPlaylist').addEventListener('click', function(){
+    playlistArray = []
+    
+    //prompt add playlist name 
+    let promptName = prompt("Name your playlist:")   
+    document.getElementById('playlist').style.visibility = "visible";
+    //document.getElementById('playlistName').innerHTML = promptName;
+     
+    //show new playlist next to all songs...
+    
+    console.log(document.querySelectorAll('input:checked'));
+    [...document.querySelectorAll('input:checked')].map(song => playlistArray.push(songLibrary[song.value]))
+    console.log(playlistArray);
+    
+    const list = document.createElement('ul');
+
+    for(let i = 0; i < playlistArray.length; i++) {
+        const item = document.createElement('li');
+        const button = document.createElement('button');
+ 
+        button.onclick = () => {playSong(i, [...playlistArray])};
+        button.innerHTML = playlistArray[i].songName;  
+         
+       
+        item.appendChild(button)
+        list.appendChild(item)
+    }
+    //document.getElementById('songList').appendChild(createSongList());
+    document.getElementById('playlist').innerHTML = "";
+
+    const h3 = document.createElement("h3")
+    h3.innerHTML = promptName;
+    document.getElementById('playlist').appendChild(h3);
+
+    document.getElementById('playlist').appendChild(list);
+})
 
 //Play song when clicked
-function playSong(index){
+function playSong(index, sourceArray,  flag){
 //songList.addEventListener('click', function(e){
     //console.log(e);
    // const clickedItem = e.target
+    isPlayingFromPlaylist = flag;
+
     const source = document.getElementById('source')
-    const songName = songLibrary[index].songName;
+    const songName = sourceArray[index].songName;
+    const songArtist = sourceArray[index].songArtist;
+   // isPlayingFromPlaylist ? playlistArray[index].songName : songLibrary[index].songName;   //ternary operator
+
     //source.src = 'music/' + clickedItem.innerText + '.mp3';
     source.src = `music/${songName}.mp3`;
     //source.src = `music/${clickedItem}.mp3`;
+    currentPlaylistSongIndex = index;
     currentSongIndex = index;
     console.log(songName)
 
     document.getElementById('currentlyPlayingSong').innerText = "Currently Playing:"
-    document.getElementById('currentSong').innerText = songName;
+    document.getElementById('currentSong').innerText = `${songName} by ${songArtist}`;
     
     audio.load()
     audio.play()
@@ -120,16 +174,31 @@ pauseAudio.onclick = () => {
 
 // //Next Song
 nextSong.onclick = () => {
-    currentSongIndex = currentSongIndex +1
+   
+    const arrayLength = isPlayingFromPlaylist ? playlistArray.length : songLibrary.length;
 
-    if (currentSongIndex > songLibrary.length -1){       //loops to first song in array
-        currentSongIndex = 0
+    if (isPlayingFromPlaylist){
+        currentPlaylistSongIndex = currentPlaylistSongIndex +1
+         if (currentPlaylistSongIndex > arrayLength -1){       //loops to first song in array
+        currentPlaylistSongIndex = 0
     }
+    } else{
+        currentSongIndex = currentSongIndex +1
+        if (currentSongIndex > arrayLength -1){       //loops to first song in array
+            currentSongIndex = 0
+        }
+    }
+
+    // if (currentSongIndex > arrayLength -1){       //loops to first song in array
+    //     currentSongIndex = 0
+    //     currentPlaylistSongIndex = 0
+    // }
     const source = document.getElementById('source')
-    source.src = `music/${songLibrary[currentSongIndex].songName}.mp3`;
+    const songName = isPlayingFromPlaylist ? playlistArray[currentPlaylistSongIndex].songName : songLibrary[currentSongIndex].songName;  
+    source.src = `music/${songName}.mp3`;
 
     document.getElementById('currentlyPlayingSong').innerText = "Currently Playing:"
-    document.getElementById('currentSong').innerText = songLibrary[currentSongIndex].songName;
+    document.getElementById('currentSong').innerText = songName;
     audio.load()
     audio.play()
 }
@@ -194,7 +263,8 @@ function formatTime(secs) {
       sec  = "0" + sec;
     }
   
-    return min + ':' + sec;
+ 
+    return isNaN(min)?"00:00":min + ':' + sec
   }
 
 // update progress bar every 1/2 second
@@ -212,7 +282,8 @@ function resetTimer(){
   console.log("Clearing timer because of activity");
   timeout = setTimeout(function(){
     document.body.style.backgroundColor = "grey";
-    alert(`Idle Mode: You are currently listening to ${songLibrary[currentSongIndex].songName}`);
+    const songName = isPlayingFromPlaylist ? playlistArray[currentPlaylistSongIndex].songName : songLibrary[currentSongIndex].songName; 
+    alert(`Idle Mode: You are currently listening to ${songName}`);
     
     //Trigger your popup after 30 seconds
   }, 30000);      
